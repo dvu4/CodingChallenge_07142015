3e91f35fb8aaef97f53a435a5b2bf4f9f00afc0c
Duc Vu <dvu4@uic.edu>
2015-07-12 04:53:27 -0500

    first commit

--- run.sh
+++ run.sh
@@ -5,3 +5,6 @@
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
 python ./src/words_tweeted.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt
 python ./src/median_unique.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt
+
+python ./src/words_tweeted.py ./tweet_input/tweets_test.txt ./tweet_output/ft11.txt
+python ./src/median_unique.py ./tweet_input/tweets_test.txt ./tweet_output/ft22.txt
